d40bf2617a61bab4ebf1b7c593617c445940225b
Agora Dev (auto) <noreply@agoraedu.local>
2026-04-22 10:22:39 +0200

    chore: allow external image hosts for next/image (img.youtube.com, cdn.sanity.io, picsum.photos)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
